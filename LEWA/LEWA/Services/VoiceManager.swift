42046eb2bf0879bde4e40ff79db15d84579287f2
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-06-28 08:36:33 +0300

    LEWA

--- LEWA/LEWA/Services/VoiceManager.swift
+++ LEWA/LEWA/Services/VoiceManager.swift
@@ -8,23 +8,11 @@
 import AVFAudio
 
 class VoiceManager {
+    
     static func speak(word: String) {
-        let voiceIdentifiers = [
-            "com.apple.ttsbundle.Daniel-compact", // Erkek
-            "com.apple.ttsbundle.Kate-compact"    // Kadın
-        ]
-        
-        let randomIndex = Int.random(in: 0..<voiceIdentifiers.count)
-        let chosenVoiceId = voiceIdentifiers[randomIndex]
-        
         let utterance = AVSpeechUtterance(string: word)
-        if let voice = AVSpeechSynthesisVoice(identifier: chosenVoiceId) {
-            utterance.voice = voice
-        } else {
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        }
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-UK")
         utterance.rate = 0.5
-        
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(utterance)
     }
